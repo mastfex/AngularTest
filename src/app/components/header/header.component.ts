5e3afbd9c9243a020dca789a97fa2c1c275a0916
Felipe Rubin Gallardo <frubin44@gmail.com>
2021-08-02 23:18:18 -0400

    Se añade ngFor

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,28 @@
 import{Component} from '@angular/core'
-
+import{Paginas} from './class/Paginas'
 @Component({
   selector :'app-header',
-  template: `<h1>Header component</h1>`
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
+  private paginas= new Array<Paginas>();
+  public constructor(){
+
+    this.paginas = [
+                    new Paginas('Inicio','Index.html'),
+                    new Paginas('Juegos','Juegos.html'),
+                    new Paginas('Consolas','Consolas.html'),
+                    new Paginas('Contacto','Contacto.html')
+                    ];
+  }
+
+  public setPaginas(paginas:Array<Paginas>){
+    this.paginas=paginas;
+  }
+
+  public getPaginas(){
+     return this.paginas;
+  }
 
 }
