e5d1549f371e0ff8b51f90dabb5eafdab00f25de
Felipe Rubin Gallardo <frubin44@gmail.com>
2021-08-03 23:23:48 -0400

    fix clases

--- src/app/components/body/clientInformation/clientInfo.component.ts
+++ src/app/components/body/clientInformation/clientInfo.component.ts
@@ -12,9 +12,9 @@ export class ClientInformationTemplate{
 
   constructor(){
     this.listaClientes=[
-      new Cliente('17315306-6','Felipe','Rubin','32','02-03-1990')
+      new Cliente('17315306-6','Felipe','Rubin','31','02-03-1990')
     ,
-      new Cliente('18315306-7','Felipe','Rubin','32','02-03-1990')
+      new Cliente('18315306-7','Christian','Ramirez','23','02-03-1995')
     ];
 
   }
@@ -32,28 +32,35 @@ export class ClientInformationTemplate{
   public verInfoCliente(){
 
     var seleccion = (document.getElementById('optionlist')as HTMLSelectElement).value;
-    var div = (document.getElementById('infoPersona')as HTMLSelectElement);
-    var h3= document.createElement('h3');
-    var ul= (document.getElementById('info')as HTMLSelectElement);
+    var div = (document.getElementById('infoPersona')as HTMLDivElement);
+    var h3= document.createElement('h3') as HTMLHeadingElement;
+    var ul= (document.getElementById('info')as HTMLUListElement);
+
+    if(div.childElementCount>0){
+      div.innerHTML='';
+      ul.innerHTML='';
+    }
 
     for(var x=0; x<this.getListaClientes().length ; x++){
         console.log(seleccion);
         if(this.getListaClientes()[x].getRut()==seleccion){
             h3.textContent="Detalle Persona";
             div.appendChild(h3);
-            var li= document.createElement('li');
+            var li= document.createElement('li') as HTMLLIElement;
             li.innerHTML= "Nombre: "+this.getListaClientes()[x].getNombre();
             var li2= document.createElement('li');
             li2.innerHTML= "Apellido: "+this.getListaClientes()[x].getApellido();
             var li3= document.createElement('li');
             li3.innerHTML= "Edad: "+this.getListaClientes()[x].getEdad();
+            var li4= document.createElement('li');
+            li4.innerHTML= "Fecha Nacimiento: "+this.getListaClientes()[x].getFechaNacimiento();
             ul.appendChild(li);
             ul.appendChild(li2);
             ul.appendChild(li3);
+            ul.appendChild(li4);
         }
     }
     div.appendChild(ul);
-
   }
 
   public getListaClientes(){
